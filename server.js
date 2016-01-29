54314c2a8f95f5e941e0aab60d675c41c93737f5
Dan Deller <dandeller@Dan-Deller-MacBook-Pro.local>
2016-01-28 16:19:56 -0800

    index.html in place. working on gulp-babel

--- server.js
+++ server.js
@@ -3,14 +3,24 @@ var app = express();
 var router = express.Router();
 var path = require('path');
 var bodyParser = require('body-parser');
+var requireDir = require('require-dir');
 var _ = require('lodash');
 
 app.get('/', function(req, res) {
-  res.send('hello dan');
+  res.sendFile(path.join(__dirname + '/public/index.html'));
 });
+
+app.use(express.static(path.join(__dirname, './public')))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+// _.each(endpoints,function(middleware, name) {
+//   app.use(middleware);
+// });
 
 var server = app.listen(3000, function() {
   var port = server.address().port;
   var host = server.address.address;
   console.log('App listening at http://%s:%s', host, port)
 });
+
+module.exports = app;
