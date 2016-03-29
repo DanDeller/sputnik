b49994aedffe87092a5ccbb2215b4ce77e48da60
Daniel Deller <deller26@Daniels-MacBook-Pro.local>
2016-03-28 19:06:34 -0700

    testing GET method with mocha test

---
+++ test/test.js
@@ -0,0 +1,22 @@
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var should = chai.should();
+var app = require('../server.js');
+
+chai.use(chaiHttp);
+
+describe('api', function() {
+	describe('endPoints', function() {
+
+		it('should return 200 on GET request', function(done) {
+			chai.request(app)
+			.get('/people')
+			.end(function(err, res) {
+				res.should.have.status(200);
+				if (err) return done(err);
+				done();
+			});
+		});
+
+	});
+});
