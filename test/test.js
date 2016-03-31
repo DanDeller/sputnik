6981af3a8c8e2c857e4e536afa9d1b86dbf70cd7
Dan Deller <dandeller@Dan-Deller-MacBook-Pro.local>
2016-03-31 15:40:13 -0700

    added test for POST

--- test/test.js
+++ test/test.js
@@ -8,9 +8,21 @@ chai.use(chaiHttp);
 describe('api', function() {
 	describe('endPoints', function() {
 
+		// GET request - should return 200 status code
 		it('should return 200 on GET request', function(done) {
 			chai.request(app)
 			.get('/people')
+			.end(function(err, res) {
+				res.should.have.status(200);
+				if (err) return done(err);
+				done();
+			});
+		});
+
+		// POST request - should return 200 status code
+		it('should return 200 on POST request', function(done) {
+			chai.request(app)
+			.post('/people')
 			.end(function(err, res) {
 				res.should.have.status(200);
 				if (err) return done(err);
