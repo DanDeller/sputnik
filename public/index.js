42f9a1fd555bf22bb07f7a599b0318452c7acae2
Daniel Deller <deller26@Daniels-MacBook-Pro.local>
2016-05-23 07:28:00 -0700

    working on implementing routes

--- public/index.js
+++ public/index.js
@@ -1,10 +1,10 @@
-import React from 'react'
-import { render } from 'react-dom'
-import App from './app/main'
-import { Router, Route, hashHistory } from 'react-router'
+import About from '/app/About'
+import Repos from '/app/Repos'
 
 render((
   <Router history={hashHistory}>
     <Route path="/" component={App}/>
+    <Route path="/repos" component={Repos}/>
+    <Route path="/about" component={About}/>
   </Router>
 ), document.getElementById('app'))
