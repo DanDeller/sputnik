8d30e5f8cbc7ed5ee027992b1ec29dca8ff77b65
Dan Deller <dandeller@Dan-Deller-MacBook-Pro.local>
2016-08-11 15:53:26 -0700

    tests failing due to error in server/endpoints/api.js file

--- server.js
+++ server.js
@@ -7,8 +7,14 @@ const webpackMiddleware = require('webpack-dev-middleware');
 const webpackHotMiddleware = require('webpack-hot-middleware');
 const config = require('./webpack.config.js');
 
+const bodyParser = require('body-parser');
+const requireDir = require('require-dir');
+const _ = require('lodash');
+const endpoints = requireDir('./server/lib/endpoints');
+const serverConfig = require('./serverConfig');
+
 const isDeveloping = process.env.NODE_ENV !== 'production';
-const port = isDeveloping ? 3000 : process.env.PORT;
+const port = isDeveloping ? 3000 : serverConfig.server.port
 const app = express();
 
 if (isDeveloping) {
@@ -28,18 +34,21 @@ if (isDeveloping) {
 
   app.use(middleware);
   app.use(webpackHotMiddleware(compiler));
-  app.get('*', function response(req, res) {
-    res.write(middleware.fileSystem.readFileSync(path.join(__dirname, 'dist/index.html')));
+  app.get('/', function response(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
     res.end();
   });
 } else {
-  app.use(express.static(__dirname + '/dist'));
-  app.get('*', function response(req, res) {
+  app.get('/', function response(req, res) {
     res.sendFile(path.join(__dirname, 'dist/index.html'));
   });
 }
 
-app.listen(port, '0.0.0.0', function onStart(err) {
+_.each(endpoints,function(name) {
+  app.use(name);
+});
+
+app.listen(serverConfig.server.port, function onStart(err) {
   if (err) {
     console.log(err);
   }
--- server/lib/models/People.js
+++ server/lib/models/People.js
@@ -1,5 +1,5 @@
 var r = require('rethinkdb');
-var config = require('../../../config');
+var config = require('../../../serverConfig');
 var _ = require('lodash');
 
 module.exports = {
@@ -87,45 +87,3 @@ module.exports = {
 		});
 	}
 };
-
-
-// exports.list = function(request, callback) {
-// 	r.table('people').run()
-// 	.then(function(response) {
-// 		callback(null, response);
-// 	}).error(function(error) {
-// 		callback(error);
-// 	});
-// };
-
-// exports.post = function(request, callback) {
-// 	var currentPerson = request.body;
-// 	r.table('people').insert({
-// 		name: currentPerson.name
-// 	}).then(function(response) {
-// 		callback(null, response);
-// 	}).error(function(error) {
-// 		callback(error);
-// 	});
-// };
-
-// exports.patch = function(request, callback) {
-// 	var query = _.extend(request.body, request.params, request.query);
-// 	var id = query.id;
-// 	r.table('people').get(id).update(query).run()
-// 	.then(function(response) {
-// 		callback(null, response);
-// 	}).error(function(error) {
-// 		callback(error);
-// 	});
-// };
-
-// exports.delete = function(request, callback) {
-// 	var currentId = request.query;
-// 	r.table('people').get(currentId.id).delete().run()
-// 	.then(function(response) {
-// 		callback(null, response);
-// 	}).error(function(error) {
-// 		callback(error);
-// 	});
-// };
