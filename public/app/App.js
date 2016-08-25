11f322c7c5416cd4a3bf188ef4acd103f93f62a7
Dan Deller <dandeller@Dan-Deller-MacBook-Pro.local>
2016-08-25 14:07:33 -0700

    adding styles to work with webpack

--- public/app/App.js
+++ public/app/App.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import styles from './App.css';
+// import React from 'react';
+// import styles from './App.css';
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {test: 'foo young'};
-    console.log(this.state);
-  }
-  render() {
-    return (
-      <div className={styles.app}>
-        bar
-      </div>
-    );
-  }
-}
+// export default class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {test: 'foo young'};
+//     console.log(this.state);
+//   }
+//   render() {
+//     return (
+//       <div className={styles.app}>
+//         bar
+//       </div>
+//     );
+//   }
+// }
 
--- public/app/main.js
+++ public/app/main.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.js';
+// import App from './App.js';
+import App from './index.jsx'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('app'));
