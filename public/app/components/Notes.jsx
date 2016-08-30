bf2945be87d4786cb48e3b3b74fcf8547daa3bc8
Daniel Deller <deller26@Daniels-MacBook-Pro.local>
2016-08-30 13:09:43 -0700

    adding new component

--- public/app/components/Notes.jsx
+++ public/app/components/Notes.jsx
@@ -1,16 +1,6 @@
 import React from 'react';
-import uuid from 'uuid';
 
-const notes = [{
-    id: uuid.v4(),
-    task: 'Learn React'
-  },
-  {
-    id: uuid.v4(),
-    task: 'Learn react some more!'
-}];
-
-export default () => ( <ul>{notes.map(note =>
+export default ({notes}) => ( <ul>{notes.map(note =>
     <li key={note.id}>{note.task}</li>
   )}</ul>
 )
