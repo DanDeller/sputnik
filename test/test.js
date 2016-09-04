1397b3f9e9fc3ea07dc81e060e08c6784e467c90
Daniel Deller <deller26@Daniels-MacBook-Pro.local>
2016-09-04 12:48:23 -0700

    test

--- test/test.js
+++ test/test.js
@@ -8,6 +8,7 @@ import {expect} from 'chai';
 
 import Note from '../public/app/components/Note';
 import Notes from '../public/app/components/Notes';
+const classnames = require('classnames');
 
 // used to call mount() before global document is loaded
 const jsdom = require('jsdom');
