b5f23727abf2390a49c7b2eb218d1306a3eb984e
Daniel Deller <deller26@Daniels-MacBook-Pro.local>
2016-09-04 10:37:57 -0700

    getting mocha tests to pass

--- public/app/components/App.jsx
+++ public/app/components/App.jsx
@@ -5,7 +5,6 @@ import Notes from './Notes';
 export default class App extends React.Component {
 	constructor(props) {
 		super(props);
-
 		this.state = {
 			notes: [
 			{
--- public/app/components/Editable.jsx
+++ public/app/components/Editable.jsx
@@ -7,27 +7,27 @@ export default ({editing, value, onEdit, ...props}) => {
 	return <span {...props}>{value}</span>;
 }
 
-class Edit extends React.Component {
-	render() {
-		const {value, ...props} = this.props;
-		return <input
-			type="text"
-			autoFocus={true}
-			defaultValue={value}
-			onBlur={this.finishEdit}
-			onKeyPress={this.checkEnter}
-			{...props} 
-		/>;
-	}
-	checkEnter = (e) => {
-		if(e.key === 'Enter') {
-			this.finishEdit(e);
-		}
-	}
-	finishEdit = (e) => {
-		const value = e.target.value;
-		if(this.props.onEdit) {
-			this.props.onEdit(value);
-		} 
-	}
-}
+// class Edit extends React.Component {
+// 	render() {
+// 		const {value, ...props} = this.props;
+// 		return <input
+// 			type="text"
+// 			autoFocus={true}
+// 			defaultValue={value}
+// 			onBlur={this.finishEdit}
+// 			onKeyPress={this.checkEnter}
+// 			{...props} 
+// 		/>;
+// 	}
+// 	checkEnter = (e) => {
+// 		if(e.key === 'Enter') {
+// 			this.finishEdit(e);
+// 		}
+// 	}
+// 	finishEdit = (e) => {
+// 		const value = e.target.value;
+// 		if(this.props.onEdit) {
+// 			this.props.onEdit(value);
+// 		} 
+// 	}
+// }
