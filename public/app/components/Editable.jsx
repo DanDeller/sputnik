90267c55715d092be73a423d0d127da650c8f748
Daniel Deller <deller26@Daniels-MacBook-Pro.local>
2016-09-04 11:55:37 -0700

    webpack now watching .css files

--- public/app/components/Editable.jsx
+++ public/app/components/Editable.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Editable = ({editing, value, onEdit}) => { 
 	if(editing) {
-		return <Editable.Edits value={value} onEdit={onEdit} />;
+		return <Editable.Edit value={value} onEdit={onEdit} />;
 	}
 	return <Editable.Value value={value} />; 
 };
@@ -11,31 +11,31 @@ Editable.Value = ({value, ...props}) => (
 	<span {...props}>{value}</span>
 )
 
-class Edit extends React.Component {
-	render() {
-		const {value, ...props} = this.props;
-		return <input
-			type="text"
-			autoFocus={true}
-			defaultValue={value}
-			onBlur={this.finishEdit}
-			onKeyPress={this.checkEnter}
-			{...props} 
-		/>;
-	}
-	checkEnter = (e) => {
-		if(e.key === 'Enter') {
-			this.finishEdit(e);
-		}
-	}
-	finishEdit = (e) => {
-		const value = e.target.value;
-		if(this.props.onEdit) {
-			this.props.onEdit(value);
-		} 
-	}
-}
+// class Edit extends React.Component {
+// 	render() {
+// 		const {value, ...props} = this.props;
+// 		return <input
+// 			type="text"
+// 			autoFocus={true}
+// 			defaultValue={value}
+// 			onBlur={this.finishEdit}
+// 			onKeyPress={this.checkEnter}
+// 			{...props} 
+// 		/>;
+// 	}
+// 	checkEnter = (e) => {
+// 		if(e.key === 'Enter') {
+// 			this.finishEdit(e);
+// 		}
+// 	}
+// 	finishEdit = (e) => {
+// 		const value = e.target.value;
+// 		if(this.props.onEdit) {
+// 			this.props.onEdit(value);
+// 		} 
+// 	}
+// }
 
-Editable.Edits = Edit;
+// Editable.Edit = Edit;
 
 export default Editable;
